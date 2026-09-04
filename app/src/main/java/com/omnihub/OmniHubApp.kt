ce9fd1e3280b7bd77ec058163c5f31fec6221193
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-04 20:12:35 +0100

    Wire ChatRepository, WebSessionManager, McpClient into Application

--- app/src/main/java/com/omnihub/OmniHubApp.kt
+++ app/src/main/java/com/omnihub/OmniHubApp.kt
@@ -2,8 +2,11 @@ package com.omnihub
 
 import android.app.Application
 import com.omnihub.core.OmniRouter
+import com.omnihub.history.ChatRepository
+import com.omnihub.mcp.McpClient
 import com.omnihub.providers.ProviderRegistry
 import com.omnihub.providers.impl.ProviderBootstrap
+import com.omnihub.providers.websession.WebSessionManager
 import com.omnihub.soul.SoulManager
 
 class OmniHubApp : Application() {
@@ -13,12 +16,21 @@ class OmniHubApp : Application() {
         private set
     lateinit var soul: SoulManager
         private set
+    lateinit var chatRepo: ChatRepository
+        private set
+    lateinit var webSessions: WebSessionManager
+        private set
+    lateinit var mcp: McpClient
+        private set
 
     override fun onCreate() {
         super.onCreate()
         instance = this
         registry = ProviderRegistry(this)
         soul = SoulManager(this)
+        chatRepo = ChatRepository(this)
+        webSessions = WebSessionManager(this)
+        mcp = McpClient(this)
         ProviderBootstrap.reload(this, registry)
         router = OmniRouter(registry)
     }
