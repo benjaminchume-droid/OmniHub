1eb81ef24c6fd9d9489757463c598eae169ac704
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-06 19:29:17 +0100

    Bump 1.0.8.2 spatial send

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.omnihub"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10801
-        versionName = "1.0.8.1"
+        versionCode = 10802
+        versionName = "1.0.8.2"
     }
 
     signingConfigs {
